8b1bebc71ad7e96ca14855c9eece23894f8643c3
Junichi Tokuda <tokuda@bwh.harvard.edu>
2013-05-16 16:01:43 -0400

    ENH: Initial version.

---
+++ DilateLabel/DilateLabel.cxx
@@ -0,0 +1,161 @@
+/*=========================================================================
+
+  Program:   3D Slicer DistanceMap CLI
+  Language:  C++
+  Date:      $Date: 2009-04-25 12:27:26 $
+  Version:   $Revision: 1.9 $
+
+  Copyright (c) Brigham and Women's Hospital. All rights reserved.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkCastImageFilter.h"
+
+#include "itkBinaryDilateImageFilter.h"
+#include "itkBinaryBallStructuringElement.h"
+
+#include "itkPluginUtilities.h"
+#include "DilateLabelCLP.h"
+
+#include "itkMultiplyByConstantImageFilter.h"
+
+namespace {
+
+template<class T> int DoIt( int argc, char * argv[], T )
+{
+  PARSE_ARGS;
+
+  const     unsigned int        Dimension       = 3;
+
+  typedef   T               InputPixelType;
+  typedef   T               OutputPixelType;
+
+  typedef   itk::Image< InputPixelType, Dimension > InputImageType;
+  typedef   itk::Image< OutputPixelType, Dimension > OutputImageType;
+
+  typedef   itk::ImageFileReader< InputImageType  >  ReaderType;
+  typedef   itk::ImageFileWriter< OutputImageType > WriterType;
+
+  typename ReaderType::Pointer reader = ReaderType::New();  
+  typename WriterType::Pointer writer = WriterType::New();
+
+  reader->SetFileName( inputVolume.c_str() );
+  writer->SetFileName( outputVolume.c_str() );
+ 
+  typedef itk::BinaryBallStructuringElement< typename InputImageType::PixelType, Dimension >
+    StructuringElementType;
+  StructuringElementType structuringElement;
+  structuringElement.SetRadius(radius);
+  structuringElement.CreateStructuringElement();
+ 
+  typedef itk::BinaryDilateImageFilter <InputImageType, OutputImageType, StructuringElementType>
+    BinaryDilateImageFilterType;
+ 
+  typename BinaryDilateImageFilterType::Pointer dilateFilter = BinaryDilateImageFilterType::New();
+  dilateFilter->SetDilateValue(label);
+  dilateFilter->SetInput(reader->GetOutput());
+  dilateFilter->SetKernel(structuringElement);
+
+  writer->SetInput(dilateFilter->GetOutput());
+
+  try
+    {
+    dilateFilter->Update();
+    }
+  catch (itk::ExceptionObject &err)
+    {
+    std::cerr << err << std::endl;
+    return EXIT_FAILURE ;
+    }
+
+  try
+    {
+    writer->Update();
+    }
+  catch (itk::ExceptionObject &err)
+    {
+    std::cerr << err << std::endl;
+    return EXIT_FAILURE ;
+    }
+
+  return EXIT_SUCCESS;
+}
+
+} // end of anonymous namespace
+
+
+int main( int argc, char * argv[] )
+{
+  PARSE_ARGS;
+
+  itk::ImageIOBase::IOPixelType pixelType;
+  itk::ImageIOBase::IOComponentType componentType;
+
+  try
+    {
+    itk::GetImageType (inputVolume, pixelType, componentType);
+
+    // This filter handles all types on input, but only produces
+    // signed types
+    switch (componentType)
+      {
+      case itk::ImageIOBase::UCHAR:
+        return DoIt( argc, argv, static_cast<unsigned char>(0));
+        break;
+      case itk::ImageIOBase::CHAR:
+        return DoIt( argc, argv, static_cast<char>(0));
+        break;
+      case itk::ImageIOBase::USHORT:
+        return DoIt( argc, argv, static_cast<unsigned short>(0));
+        break;
+      case itk::ImageIOBase::SHORT:
+        return DoIt( argc, argv, static_cast<short>(0));
+        break;
+      case itk::ImageIOBase::UINT:
+        return DoIt( argc, argv, static_cast<unsigned int>(0));
+        break;
+      case itk::ImageIOBase::INT:
+        return DoIt( argc, argv, static_cast<int>(0));
+        break;
+      case itk::ImageIOBase::ULONG:
+        return DoIt( argc, argv, static_cast<unsigned long>(0));
+        break;
+      case itk::ImageIOBase::LONG:
+        return DoIt( argc, argv, static_cast<long>(0));
+        break;
+      case itk::ImageIOBase::FLOAT:
+        return DoIt( argc, argv, static_cast<float>(0));
+        break;
+      case itk::ImageIOBase::DOUBLE:
+        return DoIt( argc, argv, static_cast<double>(0));
+        break;
+      case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
+      default:
+        std::cout << "unknown component type" << std::endl;
+        break;
+      }
+    }
+
+  catch( itk::ExceptionObject &excep)
+    {
+    std::cerr << argv[0] << ": exception caught !" << std::endl;
+    std::cerr << excep << std::endl;
+    return EXIT_FAILURE;
+    }
+  return EXIT_SUCCESS;
+}
